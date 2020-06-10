6c69e81f6d1da6ec5f01c7b939a157b5c450575e
Mario Torres <48029021+marito07@users.noreply.github.com>
2020-06-10 19:50:54 +0200

    Estructura del fichero main.js

--- 01 - Aplicacion/static/js/main.js
+++ 01 - Aplicacion/static/js/main.js
@@ -114,79 +114,12 @@ $(function () {
     $('#resultado').append('<h5 class="mt-3 mb-3"> Pertenece a la clase: ' + resultado + '</h5>')
   });
 
-  /* BOTON METODO DE BAYES ---------------------------------*/
-  $('#startBayes').click(function (event) { // Aqui se encuentra la implementacion del metodo de Bayes
-     //Desactivacion de los botones
-    $('#startKMedias').prop('disabled', true);
-    $('#startBayes').prop('disabled', true);
-    $('#startlloyd').prop('disabled', true);
-
-    /* Calculo de las medias */
-    $('#resultado').append('<h4> COMIENZO DEL METODO DE BAYES </h4>');
-
-    for (let i = 0; i < irisSetosa[0].length; i++) { // Suma de Iris-Setosa (Se suman a la vez dado que tienen el mismo numero de elementos)
-      let auxSetosa = 0;
-      let auxVersiColor = 0;
-      for (let j = 0; j < irisSetosa.length; j++) {
-        auxSetosa += irisSetosa[j][i];
-        auxVersiColor += irisVersicolor[j][i];
-      }
-      mediaSetosa.push(auxSetosa * (1 / irisSetosa.length));
-      mediaVersiColor.push(auxVersiColor * (1 / irisVersicolor.length));
-    }
-    console.log('Media de Iris-Setosa ----------')
-    console.log(mediaSetosa);
-    $('#resultado').append('<pre> Media Iris-Setosa: ' + JSON.stringify(mediaSetosa, function (key, val) {
-      return val.toFixed ? Number(val.toFixed(3)) : val;
-    }, 7) + '</pre>');
-
-    console.log('Media de Iris-VersiColor ----------')
-    console.log(mediaVersiColor);
-    $('#resultado').append('<pre> Media Iris-VersiColor: ' + JSON.stringify(mediaVersiColor, function (key, val) {
-      return val.toFixed ? Number(val.toFixed(3)) : val;
-    }, 7) + '</pre>');
-
-    let restoSetosa = [];
-    let restoVersiColor = [];
-    for (let i = 0; i < mediaSetosa.length; i++) {
-      restoSetosa.push(dataEjemplo[i] - mediaSetosa[i]);
-      restoVersiColor.push(dataEjemplo[i] - mediaVersiColor[i]);
-    }
-    let d_Setosa = 0;
-    let d_VersiColor = 0;
-    for (let i = 0; i < restoSetosa.length; i++) {
-      d_Setosa += Math.pow(restoSetosa[i], 2);
-      d_VersiColor += Math.pow(restoVersiColor[i], 2);
-    }
-
-    console.log('Result Setosa -----');
-    console.log(d_Setosa);
-    $('#resultado').append('<h4> Distancia Iris-Setosa: ' + d_Setosa + '</h4>')
-
-    console.log('Result VersiColor -----');
-    console.log(d_VersiColor);
-    $('#resultado').append('<h4> Distancia Iris-VersiColor: ' + d_VersiColor + '</h4>')
-
-
-
-    var resultado = '';
-    if (d_Setosa < d_VersiColor) {
-      resultado = 'Iris-setosa';
-    }
-    else {
-      resultado = 'Iris-versicolor';
-    }
-
-
-    console.log('El resultado es: ' + resultado);
-    $('#resultado').append('<h5 class="mt-3 mb-3"> Pertenece a la clase: ' + resultado + '</h5>')
-  });
-
   /* FUNCIONES ASISTENTES PARA EL METODO K-MEDIAS */
   function k_medias() { //Funcion del Metodo de Ordenacion de KMedias
     var salir = false; // indica si se han llegado a las condiciones
     var iteracion = 1;
     while (!salir) { // Hace el numero de iteraciones hasta que se hayan cumplido las condiciones.
+      console.log('Iteracion numero: ' + iteracion);
       const exponente = 1 / (KPesoExponencial_b - 1); // 1/(b-1)
       // Si hay el mismo numero de elementos en las 2 clases entonces basta con resolverlo 
       // para una de ellas y el resultado de la otra sera la resta de ese valor a 1
@@ -294,14 +227,11 @@ $(function () {
     let i = 0;
     var aux1 = 0;
     $('#resultado').append('<h6> Aplicacion del criterio de convergencia: <h6>');
-    console.log('Creterio de convergencia: ' + centrosSetosa.length);
     while (i < centrosSetosa.length) { //Comprueba el centro de Iris-Setosa
       var value = Math.pow(nuevoCentroSetosa[i] - centrosSetosa[i], 2);
-      console.log('Nuevo centro: ' + value);
       aux1 += Math.pow(nuevoCentroSetosa[i] - centrosSetosa[i], 2);
       i++;
     }
-    console.log(Math.sqrt(aux1))
     if (Math.sqrt(aux1) > KTolerancia) {
       $('#resultado').append('<strong> Iris Setosa: </strong><br>');
       $('#resultado').append(Math.sqrt(aux1) + ' > ' + KTolerancia + '<br>');
@@ -317,7 +247,6 @@ $(function () {
         aux2 += Math.pow(nuevoCentroVersi[j] - centrosVersi[j], 2);
         j++;
       }
-      console.log(Math.sqrt(aux2));
       if (Math.sqrt(aux2) > KTolerancia) {
         $('#resultado').append('<strong> Iris Versicolor: </strong><br>');
         $('#resultado').append(Math.sqrt(aux2) + ' > ' + KTolerancia + '<br>');
@@ -347,4 +276,73 @@ $(function () {
       return 'Iris-versicolor';
     }
   }
+
+
+    /* BOTON METODO DE BAYES ---------------------------------*/
+    $('#startBayes').click(function (event) { // Aqui se encuentra la implementacion del metodo de Bayes
+      //Desactivacion de los botones
+     $('#startKMedias').prop('disabled', true);
+     $('#startBayes').prop('disabled', true);
+     $('#startlloyd').prop('disabled', true);
+ 
+     /* Calculo de las medias */
+     $('#resultado').append('<h4> COMIENZO DEL METODO DE BAYES </h4>');
+ 
+     for (let i = 0; i < irisSetosa[0].length; i++) { // Suma de Iris-Setosa (Se suman a la vez dado que tienen el mismo numero de elementos)
+       let auxSetosa = 0;
+       let auxVersiColor = 0;
+       for (let j = 0; j < irisSetosa.length; j++) {
+         auxSetosa += irisSetosa[j][i];
+         auxVersiColor += irisVersicolor[j][i];
+       }
+       mediaSetosa.push(auxSetosa * (1 / irisSetosa.length));
+       mediaVersiColor.push(auxVersiColor * (1 / irisVersicolor.length));
+     }
+     console.log('Media de Iris-Setosa ----------')
+     console.log(mediaSetosa);
+     $('#resultado').append('<pre> Media Iris-Setosa: ' + JSON.stringify(mediaSetosa, function (key, val) {
+       return val.toFixed ? Number(val.toFixed(3)) : val;
+     }, 7) + '</pre>');
+ 
+     console.log('Media de Iris-VersiColor ----------')
+     console.log(mediaVersiColor);
+     $('#resultado').append('<pre> Media Iris-VersiColor: ' + JSON.stringify(mediaVersiColor, function (key, val) {
+       return val.toFixed ? Number(val.toFixed(3)) : val;
+     }, 7) + '</pre>');
+ 
+     let restoSetosa = [];
+     let restoVersiColor = [];
+     for (let i = 0; i < mediaSetosa.length; i++) {
+       restoSetosa.push(dataEjemplo[i] - mediaSetosa[i]);
+       restoVersiColor.push(dataEjemplo[i] - mediaVersiColor[i]);
+     }
+     let d_Setosa = 0;
+     let d_VersiColor = 0;
+     for (let i = 0; i < restoSetosa.length; i++) {
+       d_Setosa += Math.pow(restoSetosa[i], 2);
+       d_VersiColor += Math.pow(restoVersiColor[i], 2);
+     }
+ 
+     console.log('Result Setosa -----');
+     console.log(d_Setosa);
+     $('#resultado').append('<h4> Distancia Iris-Setosa: ' + d_Setosa + '</h4>')
+ 
+     console.log('Result VersiColor -----');
+     console.log(d_VersiColor);
+     $('#resultado').append('<h4> Distancia Iris-VersiColor: ' + d_VersiColor + '</h4>')
+ 
+ 
+ 
+     var resultado = '';
+     if (d_Setosa < d_VersiColor) {
+       resultado = 'Iris-setosa';
+     }
+     else {
+       resultado = 'Iris-versicolor';
+     }
+ 
+ 
+     console.log('El resultado es: ' + resultado);
+     $('#resultado').append('<h5 class="mt-3 mb-3"> Pertenece a la clase: ' + resultado + '</h5>')
+   });
 });
